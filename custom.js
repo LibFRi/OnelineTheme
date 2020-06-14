25f8ca9d7042ba6a95b95923799639cf688b81ed
Libfrichard <libfrichard@gmail.com>
2020-06-14 14:55:11 +0800

    add new feature: move extension toolbar to panel

---
+++ custom.js
@@ -0,0 +1,12 @@
+/* move extension toolbar to panel wrapper */
+setTimeout(function wait() {
+    const browser = document.getElementById('browser');
+    if (browser) {
+        const toolbarExtensions = document.querySelector('.toolbar-extensions');
+        document.querySelector('.addwebpanel-wrapper').prepend(toolbarExtensions);
+    }
+    else {
+        setTimeout(wait, 300);
+    }
+}, 300);
+
